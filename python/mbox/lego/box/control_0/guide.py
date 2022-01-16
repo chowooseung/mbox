d94a466e4f43f3d3dad1123f848fa223c7163505
chowooseoung <chowooseoung@gmail.com>
2022-01-16 17:38:01 +0900

    update

--- python/mbox/lego/box/control_0/guide.py
+++ python/mbox/lego/box/control_0/guide.py
@@ -105,6 +105,8 @@ class Block(SubBlock):
 
         name_format = f"{self['comp_name']}_{self['comp_side']}{self['comp_index']}_%s"
         if isinstance(self.parent, RootBlock) or self.parent is None:
+            print(self)
+            print(self.top)
             parent = self.top.network.attr("guide").inputs(type="transform")[0]
         else:
             parent = self.parent.network.attr("transforms").inputs(type="transform")[-1]
